a1c771c834b9bdff8df6ff5447ff789ae2e31ebb
Vinícius <vinileardini@gmail.com>
2023-10-16 16:54:47 -0300

    dark/light mode e inicio conversor

--- exec.js
+++ exec.js
@@ -41,6 +41,9 @@ fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,ARS-BRL,RUB-
     else if(document.getElementById('variacaoDolar').value != 0 && document.getElementById('variacaoDolar').value < 0){
         document.getElementById('variacaoDolar').className = 'inputCotacoesVariacaoNegativa'
     }
+    else{
+        document.getElementById('variacaoDolar').className = 'inputCotacoesVariacao'
+    }
 
     document.getElementById('variacaoEuro').value = dados.EURBRL.varBid
     if(document.getElementById('variacaoEuro').value != 0 && document.getElementById('variacaoEuro').value > 0){
@@ -49,6 +52,10 @@ fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,ARS-BRL,RUB-
     else if(document.getElementById('variacaoEuro').value != 0 && document.getElementById('variacaoEuro').value < 0){
         document.getElementById('variacaoEuro').className = 'inputCotacoesVariacaoNegativa'
     }
+    else{
+        document.getElementById('varicaoEuro').className = 'inputCotacoesVariacao'
+    }
+
     document.getElementById('variacaoPeso').value = dados.ARSBRL.varBid
     if(document.getElementById('variacaoPeso').value != 0 && document.getElementById('variacaoEuro') > 0){
         document.getElementById('variacaoPeso').className = 'inputCotacoesVariacaoPositiva'
@@ -56,6 +63,10 @@ fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,ARS-BRL,RUB-
     else if(document.getElementById('variacaoPeso').value != 0 && document.getElementById('variacaoPeso').value < 0){
         document.getElementById('variacaoPeso').className = 'inputCotacoesVariacaoNegativa'
     }
+    else{
+        document.getElementById('variacaoPeso').className = 'inputCotacoesVariacao'
+    }
+
     document.getElementById('variacaoRublo').value = dados.RUBBRL.varBid
     if(document.getElementById('variacaoRublo').value != 0 && document.getElementById('variacaoRublo').value > 0){
         document.getElementById('variacaoRublo').className = 'inputCotacoesVariacaoPositiva'
@@ -63,6 +74,10 @@ fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,ARS-BRL,RUB-
     else if(document.getElementById('variacaoRublo').value != 0 && document.getElementById('variacaoRublo').value < 0){
         document.getElementById('variacaoRublo').className = 'inputCotacoesVariacaoNegativa'
     }
+    else{
+        document.getElementById('variacaoRublo').className = 'inputCotacoesVariacao'
+    }
+
     document.getElementById('variacaoBitcoin').value = dados.BTCBRL.varBid
     if(document.getElementById('variacaoBitcoin').value != 0 && document.getElementById('variacaoBitcoin').value > 0){
         document.getElementById('variacaoBitcoin').className = 'inputCotacoesVariacaoPositiva'
@@ -70,12 +85,19 @@ fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,ARS-BRL,RUB-
     else if(document.getElementById('variacaoBitcoin').value != 0 && document.getElementById('variacaoBitcoin').value > 0){
         document.getElementById('variacaoBitcoin').className = 'inputCotacoesVariacaoNegativa'
     }
+    else{
+        document.getElementById('variacaoBitcoin').className = 'inputCotacoesVariacao'
+    }
+
     document.getElementById('variacaoLibra').value = dados.GBPBRL.varBid
     if(document.getElementById('variacaoLibra').value != 0 && document.getElementById('variacaoLibra').value > 0){
         document.getElementById('variacaoLibra').className = 'inputCotacoesVariacaoPositiva'
     }
     else if(document.getElementById('variacaoLibra').value != 0 && document.getElementById('variacaoLibra').value < 0){
         document.getElementById('variacaoLibra').className = 'inputCotacoesVariacaoNegativa'
+    }
+    else{
+        document.getElementById('variacaoLibra').className = 'inputCotacoesVariacao'
     }
 
 
@@ -86,9 +108,15 @@ fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,ARS-BRL,RUB-
 
 function alteraModo(){
 
-    var elementoBody = document.body
+    elementoBody = document.body
+
+    inputs = document.querySelectorAll(".inputCotacoes");
 
     elementoBody.classList.toggle("dark-mode");
+
+    inputs.forEach(input => {
+        input.classList.toggle('inputCotacoesDark')
+    });
 
 }
 
@@ -98,46 +126,72 @@ function alteraModo(){
 function operacaoConversao(){
     valorInput1 = document.getElementById('input1').value
     valorInput2 = document.getElementById('input2').value
-    moedaSelecionada2 = document.getElementById('tipo-moeda2').value
+    moedaSelecionada2 = document.getElementById('tipo_moeda2')
 
     if(valorInput1 != 0 && valorInput1 > 0){
-        moedaSelecionada = document.getElementById('tipo-moeda1').value 
+        moedaSelecionada = document.getElementById('tipo_moeda1')
 
-        if(moedaSelecionada == 0){
+        if(moedaSelecionada.value == 0){
             //Conversão real / real
-            if(moedaSelecionada2 == 0){
+            if(moedaSelecionada2.value == 0){
                 document.getElementById('input2').value = valorInput1
             }
             //Conversão real/dolar
-            else if(moedaSelecionada2 == 1){
+            else if(moedaSelecionada2.value == 1){
                 conversaoRealDolar = valorInput1 * document.getElementById('valorDolar').value
                 document.getElementById('input2').value = conversaoRealDolar
             }
             //Conversão real/euro
-            else if(moedaSelecionada2 == 2){
+            else if(moedaSelecionada2.value == 2){
                 conversaoRealEuro = valorInput1 * document.getElementById('valorEuro').value
                 document.getElementById('input2').value = conversaoRealEuro
             }
+            //Conversão real/peso
+            else if(moedaSelecionada2.value == 3){
+                conversaoRealPeso = valorInput1 * document.getElementById('valorPeso').value
+                document.getElementById('input2').value = conversaoRealPeso
+            }
+            //Conversão real/rublo
+            else if(moedaSelecionada2.value == 4){
+                conversaoRealRublo = valorInput1 * document.getElementById('valorRublo').value
+                document.getElementById('input2').value = conversaoRealRublo
+            }
+            //Conversão real/bitcoin
+            else if(moedaSelecionada2.value == 5){
+                conversaoRealBit = valorInput1 * document.getElementById('valorBitcoin').value
+                document.getElementById('input2').value = conversaoRealBit
+            }
+            //Conversão real/libra
+            else if(moedaSelecionada2.value == 6){
+                conversaoRealLibra = valorInput1 * document.getElementById('valorLibra').value
+                document.getElementById('input2').value = conversaoRealLibra
+            }
         }
-        else if(moedaSelecionada == 1){
+        else if(moedaSelecionada.value == 1){
 
         }
-        else if(moedaSelecionada == 2){
+        else if(moedaSelecionada.value == 2){
 
         }
-        else if(moedaSelecionada == 3){
+        else if(moedaSelecionada.value == 3){
 
         }
-        else if(moedaSelecionada == 4){
+        else if(moedaSelecionada.value == 4){
 
         }
-        else if(moedaSelecionada == 5){
+        else if(moedaSelecionada.value == 5){
 
         }
-        else if(moedaSelecionada == 6){
+        else if(moedaSelecionada.value == 6){
 
         }
     }
     
+}
+
+// Função alteração tipo moeda
+
+function alteraMoeda(){
+
 }
 
